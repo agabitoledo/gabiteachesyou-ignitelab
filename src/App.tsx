56036388808b2f24bc1a6bd48eda274288f09f50
agabitoledo <gabriellefariat@gmail.como>
2022-06-22 00:04:03 -0300

    created ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,8 @@
-import { gql, useQuery } from '@apollo/client';
-// import { useEffect } from 'react';
-import { client } from './lib/apollo';
-
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons { 
-      id
-      title
-
-      teacher{
-       name
-       bio
-      }
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-
+import { Event } from "./pages/Event"
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[], }>(GET_LESSONS_QUERY)
-  console.table(data)
-
-  //USANDO O USEEFFECT
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY,
-  //   }).then(response => console.log(response.data))
-  // }, [])
-
-
-
   return (
     <>
-      <h1 className="text-5xl font-bold text-violet-500">Hello there!</h1>
-      <ul>
-        {
-          data?.lessons.map(lesson => {
-            return <li key={lesson.id}>{lesson.title}</li>
-          })
-        }
-      </ul>
+      <Event />
     </>
   )
 }
---
+++ src/pages/Event/index.tsx
@@ -0,0 +1,17 @@
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { VideoPlayer } from "../../components/VideoPlayer";
+
+export function Event() {
+    return (
+        <>
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-1">
+                <VideoPlayer />
+                <Sidebar />
+            </main>
+        </div>
+        </>
+    )
+}
